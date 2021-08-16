8249f62e392fca8d895f468795088c60e2da6e99
Jessie Wooten <jessiewootenvisual@gmail.com>
2021-08-16 15:20:49 -0500

    chore: add title

--- src/components/ui/date-chip/DateChip.tsx
+++ src/components/ui/date-chip/DateChip.tsx
@@ -22,6 +22,7 @@ export const DateChip = ({ type, date }: DateChipProps) => {
     const displayDate = type === 'due' ? addBusinessDays(new Date(date), 2) : new Date(date);
     return (
         <Chip
+            title={type === 'due' ? 'Due Date' : 'Received date'}
             icon={getIcon(type)}
             label={format(displayDate, 'MM/dd')}
             color={type === 'due' ? 'primary' : undefined}
